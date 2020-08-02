9c23272e1aa264d7fe449560abf4954866c8e59a
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-08-02 12:43:04 -0700

    Comments

--- OAT/OperationArguments.cs
+++ OAT/OperationArguments.cs
@@ -4,8 +4,18 @@ using System.Text;
 
 namespace Microsoft.CST.OAT
 {
+    /// <summary>
+    /// The class to hold arguments to pass to a clause
+    /// </summary>
     public class OperationArguments
     {
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        /// <param name="clause">The Clause this refers to</param>
+        /// <param name="state1">Object state 1</param>
+        /// <param name="state2">Object state 2</param>
+        /// <param name="captures">The previous Clause Captures</param>
         public OperationArguments(Clause clause, object? state1, object? state2, IEnumerable<ClauseCapture>? captures)
         {
             this.clause = clause;
@@ -13,9 +23,21 @@ namespace Microsoft.CST.OAT
             this.state2 = state2;
             this.captures = captures;
         }
+        /// <summary>
+        /// The clause associated with these arguments
+        /// </summary>
         public Clause clause { get; }
+        /// <summary>
+        /// The first object state
+        /// </summary>
         public object? state1 { get; }
+        /// <summary>
+        /// The second object state
+        /// </summary>
         public object? state2 { get; }
+        /// <summary>
+        /// The captures from previous clauses in the same rule.
+        /// </summary>
         public IEnumerable<ClauseCapture>? captures { get; }
     }
 }
--- OAT/ScriptData.cs
+++ OAT/ScriptData.cs
@@ -5,24 +5,47 @@ using System.Text;
 
 namespace Microsoft.CST.OAT
 {
+    /// <summary>
+    /// A holder for a Script to run for a clause
+    /// </summary>
     public class ScriptData
     {
-        public ScriptData(string code, IEnumerable<string> imports, IEnumerable<string> references)
+        /// <summary>
+        /// The constructor for a Script to run for a clause.
+        /// </summary>
+        /// <param name="code">The Code to run</param>
+        /// <param name="imports">The imports to include. For example "System.IO".</param>
+        /// <param name="references">The assembly references to include.  For example "MyAssembly" for "MyAssembly.dll"</param>
+        public ScriptData(string code, IEnumerable<string>? imports = null, IEnumerable<string>? references = null)
         {
-            Code = code;
-            Imports = imports.ToArray();
-            References = references.ToArray();
+            Code = code ?? string.Empty;
+            Imports = imports?.ToArray() ?? Array.Empty<string>();
+            References = references?.ToArray() ?? Array.Empty<string>();
             ImportString = string.Join(",", Imports);
             ReferencesString = string.Join(",", References);
             _hashCode = (Code, ImportString, ReferencesString).GetHashCode();
         }
 
+        /// <summary>
+        /// The Script code
+        /// </summary>
         public string Code { get; }
+        /// <summary>
+        /// The Script Imports
+        /// </summary>
         public string[] Imports { get; }
+        /// <summary>
+        /// The Script References
+        /// </summary>
         public string[] References { get; }
-        public string ImportString { get; }
-        public string ReferencesString { get; }
+        internal string ImportString { get; }
+        internal string ReferencesString { get; }
 
+        /// <summary>
+        /// Overridden Equality constructor
+        /// </summary>
+        /// <param name="obj">The object to compare</param>
+        /// <returns>True if obj is a ScriptData and the Code, Imports and References match.  Imports and References are order sensitive.</returns>
         public override bool Equals(object obj)
         {
             if (obj is ScriptData scriptIn)
@@ -43,6 +66,10 @@ namespace Microsoft.CST.OAT
 
         private int _hashCode;
 
+        /// <summary>
+        /// Get the HashCode for this ScriptData
+        /// </summary>
+        /// <returns>The HashCode over the Code, Imports and References. Imports and References are order sensitive.</returns>
         public override int GetHashCode()
         {
             return _hashCode;
