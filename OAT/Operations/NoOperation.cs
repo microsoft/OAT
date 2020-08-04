657396c20f6c143157baf08318c8d3223192cdb5
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-08-03 18:23:16 -0700

    Fix IsAfter issue

--- OAT/Operations/NoOperation.cs
+++ OAT/Operations/NoOperation.cs
@@ -8,29 +8,29 @@ using System.Linq;
 namespace Microsoft.CST.OAT.Operations
 {
     /// <summary>
-    /// The default NoOperation operation
+    ///     The default NoOperation operation
     /// </summary>
     public class NoOperation : OatOperation
     {
         /// <summary>
-        /// Create an OatOperation given an analyzer
+        ///     Create an OatOperation given an analyzer
         /// </summary>
-        /// <param name="analyzer">The analyzer context to work with</param>
-        public NoOperation(Analyzer analyzer) : base(Operation.IsAfter, analyzer)
+        /// <param name="analyzer"> The analyzer context to work with </param>
+        public NoOperation(Analyzer analyzer) : base(Operation.NoOperation, analyzer)
         {
             OperationDelegate = NoOperationOperationDelegate;
             ValidationDelegate = NoOperationValidationDelegate;
-        }
-
-        internal IEnumerable<Violation> NoOperationValidationDelegate(Rule rule, Clause clause)
-        {
-            yield return new Violation(string.Format(Strings.Get("Err_NoOperation_{0}{1}"), rule.Name, clause.Label ?? rule.Clauses.IndexOf(clause).ToString()), rule, clause);
         }
 
         internal OperationResult NoOperationOperationDelegate(Clause clause, object? state1, object? state2, IEnumerable<ClauseCapture>? captures)
         {
             Log.Warning("Clause {0} has Operation NoOperation.", clause.Label);
             return new OperationResult(false);
+        }
+
+        internal IEnumerable<Violation> NoOperationValidationDelegate(Rule rule, Clause clause)
+        {
+            yield return new Violation(string.Format(Strings.Get("Err_NoOperation_{0}{1}"), rule.Name, clause.Label ?? rule.Clauses.IndexOf(clause).ToString()), rule, clause);
         }
     }
 }
