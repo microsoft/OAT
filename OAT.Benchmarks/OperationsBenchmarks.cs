38860423e8ef2295cc03b2354657ec2ffea828b5
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-08-03 19:31:23 -0700

    Add Basic Benchmarks (#26)

---
+++ OAT.Benchmarks/OperationsBenchmarks.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.CST.OAT;
+using Microsoft.CST.OAT.Tests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Microsoft.CST.OAT.Tests.VehicleDemo;
+
+namespace OAT.Benchmarks
+{
+    public class OperationsBenchmarks
+    {
+        public static Rule regexRule = new Rule("Regex")
+        {
+            Clauses = new List<Clause>()
+            {
+                new Clause(Operation.Regex)
+                {
+                    Data = new List<string>()
+                    {
+                        "Magic"
+                    }
+                }
+            }
+        };
+
+        public OperationsBenchmarks()
+        {
+            Analyzer = new Analyzer();
+        }
+
+        public Analyzer Analyzer { get; }
+
+        [Benchmark]
+        public void TestMagicString()
+        {
+            Analyzer.Applies(regexRule, "This String Has Magic Inside");
+        }
+
+        [Benchmark]
+        public void TestNormalString()
+        {
+            Analyzer.Applies(regexRule, "This String Has Is Super Normal");
+        }
+    }
+}
---
+++ OAT.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Linq.Expressions;
+
+namespace OAT.Benchmarks
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ExpressionBenchmarks>();
+        }
+    }
+}
