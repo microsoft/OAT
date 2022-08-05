31295f91490508d8774bb743adda9716616a961a
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-08-04 20:31:46 -0700

    Make the GetClauseCapture api public for use with AI/DevSkim

---
+++ OAT/AnalyzerOptions.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT License.
+namespace Microsoft.CST.OAT
+{
+    /// <summary>
+    /// Holds options to instantiate an Analyzer with
+    /// </summary>
+    public class AnalyzerOptions
+    {
+        /// <summary>
+        /// The constructor
+        /// </summary>
+        /// <param name="runScripts">If running Scripts should be enabled</param>
+        /// <param name="parallel">If the analyzer should parallelize analysis</param>
+        public AnalyzerOptions(bool runScripts = false, bool parallel = true)
+        {
+            RunScripts = runScripts;
+            Parallel = parallel;
+        }
+
+        /// <summary>
+        /// If running Scripts is enabled
+        /// </summary>
+        public bool RunScripts { get; }
+        /// <summary>
+        /// Should analysis be done using parallelization
+        /// </summary>
+        public bool Parallel { get; }
+    }
+}
